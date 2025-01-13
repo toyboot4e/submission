b94fb6450023acb03a494d70d6c15823301cc13f
toyboot4e <toyboot4e@gmail.com>
2025-01-13 22:11:11 +0900

    add README.org

---
+++ flake.nix
@@ -0,0 +1,53 @@
+# nix-direnv 用
+# https://github.com/nix-community/nix-direnv
+
+# cabal-plan は license-report 付きでインストールする方法が分からなかったので
+# 手動でビルドしています……
+# https://github.com/haskell-hvr/cabal-plan
+
+{
+  description = "A basic flake with a shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+	      };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            pkg-config
+            stack
+            cabal-install
+            # glibc
+            llvmPackages.bintools
+            # openssl
+
+            # hmatrix: https://github.com/haskell-numerics/hmatrix/blob/master/shell.nix
+            zlib
+            sundials
+            blas
+            gfortran.cc
+            liblapack
+            gsl
+            glpk
+            sundials
+          ];
+
+          packages = [
+            # GHC 9.8.4
+            (haskell.compiler.ghc984.override { useLLVM = true; })
+            haskell.packages.ghc984.cabal-fmt
+            # TODO: install `cabal-plan` with `
+            # haskell.packages.ghc984.cabal-plan
+          ];
+        };
+      });
+}
